2dd20be03595d43be96a0e5c07ff7cd207e5bb73
m-kudahl <mathias.laursen97@gmail.com>
2023-04-19 15:03:48 +0200

    Lavede nogle seje ting

--- app/src/main/java/com/example/plswork/MainActivity.java
+++ app/src/main/java/com/example/plswork/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
+public class MainActivity extends AppCompatActivity {
+    AutoCompleteTextView autocomplete;
+
+    String[] cities = { "Randers", "Aalborg","Aarhus",
+            "Copenhagen"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        autocomplete = (AutoCompleteTextView)
+                findViewById(R.id.autoCompleteTextView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this,android.R.layout.select_dialog_item, cities);
+
+        autocomplete.setThreshold(2);
+        autocomplete.setAdapter(adapter);
     }
 }
