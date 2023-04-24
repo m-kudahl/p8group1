301db323b2a54715d32f23d24ec3801eb93b6e5e
athesb <rflamingw@gmail.com>
2023-04-24 09:34:57 +0200

    Database og firebase

---
+++ app/src/main/java/com/example/plswork/User.java
@@ -0,0 +1,19 @@
+package com.example.plswork;
+
+public class User {
+
+    private String email;
+    private String password;
+    private String region;
+    private String fullName;
+
+
+    public User(String email, String password, String region, String fullName) {
+        this.email = email;
+        this.password = password;
+        this.region = region;
+        this.fullName = fullName;
+    }
+
+}
+
