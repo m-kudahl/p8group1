5ffcab32246d165ca08483caf3810ae910334c3c
Miacmr <miachristina15@gmail.com>
2023-04-24 15:40:27 +0200

    mia branch

--- app/src/main/java/com/example/plswork/EducationSUActivity.java
+++ app/src/main/java/com/example/plswork/EducationSUActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class EducationActivity extends AppCompatActivity {
+public class EducationSUActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_education);
+        setContentView(R.layout.activity_education_suactivity);
     }
 }
