70fc56b6f171d5fa28e5517efdd33d38f8c94a63
AJordanJ <115153994+AJordanJ@users.noreply.github.com>
2023-04-25 22:36:17 +0200

    moremore

---
+++ app/src/main/java/com/example/plswork/BenefitsPage.java
@@ -0,0 +1,14 @@
+package com.example.plswork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BenefitsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_benefits_page);
+    }
+}
