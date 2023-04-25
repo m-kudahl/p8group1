b679c72a7eae78cd66d246252be78175b68df8ae
Veteranpit <115033980+Veteranpit@users.noreply.github.com>
2023-04-25 20:08:01 +0200

    TabDone

---
+++ app/src/main/java/com/example/plswork/ViewPagerAdapter.java
@@ -0,0 +1,28 @@
+package com.example.plswork;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0: return new BusFragment();
+            case 1: return new TrainFragment();
+            default: return new BusFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
