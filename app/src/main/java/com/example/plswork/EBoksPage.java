1bd2ef23f4fb2c234ad3a03c30714e1172948d62
athesb <rflamingw@gmail.com>
2023-04-26 10:34:24 +0200

    hej

--- app/src/main/java/com/example/plswork/EBoksPage.java
+++ app/src/main/java/com/example/plswork/EBoksPage.java
@@ -1,6 +1,7 @@
 package com.example.plswork;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,10 @@ public class EBoksPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eboks_page);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.inflateMenu(R.menu.main_menu);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle(null);
     }
 }
