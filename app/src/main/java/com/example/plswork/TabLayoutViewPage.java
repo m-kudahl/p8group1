ff344259ecbf6ed734940d6a7c4561230d866c57
athesb <rflamingw@gmail.com>
2023-04-26 12:10:11 +0200

    hej

---
+++ app/src/main/java/com/example/plswork/TabLayoutViewPage.java
@@ -0,0 +1,36 @@
+package com.example.plswork;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class TabLayoutViewPage extends FragmentStateAdapter{
+
+    public TabLayoutViewPage(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch(position){
+            case 0:
+                return new EssentialsTab();
+
+            case 1:
+                return new EverydayTap();
+            default: return new  EssentialsTab();
+        }
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+
+}
---
+++ app/src/main/java/com/example/plswork/Tab_Layout.java
@@ -0,0 +1,55 @@
+package com.example.plswork;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class Tab_Layout extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    TabLayoutViewPage viewPagerAdapter;
+
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewPager);
+        viewPagerAdapter = new TabLayoutViewPage(this);
+        viewPager2.setAdapter(viewPagerAdapter);
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+
+            }
+        });
+    }
+}
