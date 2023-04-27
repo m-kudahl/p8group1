1615b330af34ea15f94f7fd20b78a8b53530fcf1
AJordanJ <115153994+AJordanJ@users.noreply.github.com>
2023-04-27 15:37:34 +0200

    nice

--- app/src/main/java/com/example/plswork/EBoksPage.java
+++ app/src/main/java/com/example/plswork/EBoksPage.java
@@ -3,10 +3,15 @@ package com.example.plswork;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class EBoksPage extends AppCompatActivity {
 
+    ImageButton appleButton, googleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +21,29 @@ public class EBoksPage extends AppCompatActivity {
         myToolbar.inflateMenu(R.menu.main_menu);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setTitle(null);
+
+        appleButton = findViewById(R.id.appleStoreButtonEBoks);
+        googleButton = findViewById(R.id.googleStoreButtonEBoks);
+        appleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://apps.apple.com/dk/app/e-boks-dk/id482926022";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://play.google.com/store/apps/details?id=com.eboks.activities&hl=da&gl=US&pli=1";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/plswork/MitIDpage.java
+++ app/src/main/java/com/example/plswork/MitIDpage.java
@@ -2,15 +2,44 @@ package com.example.plswork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MitIDpage extends AppCompatActivity {
 
+    ImageButton appleButton, googleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mit_idpage);
-    }
 
+
+        appleButton = findViewById(R.id.appleStoreButtonMitID);
+        googleButton = findViewById(R.id.googleStoreButtonMitID);
+
+        appleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://apps.apple.com/dk/app/e-boks-dk/id482926022";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://play.google.com/store/apps/details?id=com.eboks.activities&hl=da&gl=US&pli=1";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+    }
 
 }
