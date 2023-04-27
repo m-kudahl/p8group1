14d4448e6275cadd66468a7ca0c21e1cfdb98e12
m-kudahl <mathias.laursen97@gmail.com>
2023-04-27 22:47:54 +0200

    LANDING

--- app/src/main/java/com/example/plswork/LoginActivity.java
+++ app/src/main/java/com/example/plswork/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.plswork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn = (Button)findViewById(R.id.createaccountButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
     }
 }
