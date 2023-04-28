cbc36dd6eedb39a5bd22f9771e9dfc6c4342f1a2
athesb <rflamingw@gmail.com>
2023-04-28 10:45:39 +0200

    BenefitsPage.java og LandingActivity.java er fikset

--- app/src/main/java/com/example/plswork/BenefitsPage.java
+++ app/src/main/java/com/example/plswork/BenefitsPage.java
@@ -1,22 +1,15 @@
+
 package com.example.plswork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-<<<<<<<< HEAD:app/src/main/java/com/example/plswork/BenefitsPage.java
 public class BenefitsPage extends AppCompatActivity {
-========
-public class LandingActivity extends AppCompatActivity {
->>>>>>>> origin/Mathias:app/src/main/java/com/example/plswork/LandingActivity.java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<<< HEAD:app/src/main/java/com/example/plswork/BenefitsPage.java
         setContentView(R.layout.activity_benefits_page);
-========
-        setContentView(R.layout.activity_landing);
->>>>>>>> origin/Mathias:app/src/main/java/com/example/plswork/LandingActivity.java
     }
 }
--- app/src/main/java/com/example/plswork/LandingActivity.java
+++ app/src/main/java/com/example/plswork/LandingActivity.java
@@ -4,19 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-<<<<<<<< HEAD:app/src/main/java/com/example/plswork/BenefitsPage.java
-public class BenefitsPage extends AppCompatActivity {
-========
 public class LandingActivity extends AppCompatActivity {
->>>>>>>> origin/Mathias:app/src/main/java/com/example/plswork/LandingActivity.java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<<< HEAD:app/src/main/java/com/example/plswork/BenefitsPage.java
-        setContentView(R.layout.activity_benefits_page);
-========
         setContentView(R.layout.activity_landing);
->>>>>>>> origin/Mathias:app/src/main/java/com/example/plswork/LandingActivity.java
     }
 }
