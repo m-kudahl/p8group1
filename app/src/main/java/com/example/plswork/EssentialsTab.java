44d6e502345ce45baad96d871b61cdd4ff21e049
Alona90 <DS26DD@student.aau.dk>
2023-04-28 09:53:13 +0200

    ,fkjbh.xf

--- app/src/main/java/com/example/plswork/EssentialsTab.java
+++ app/src/main/java/com/example/plswork/EssentialsTab.java
@@ -1,7 +1,11 @@
 package com.example.plswork;
 
+import static com.example.plswork.R.id.cardEducation;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,14 +15,64 @@ import android.view.ViewGroup;
 
 public class EssentialsTab extends Fragment {
 
-
+    CardView cardViewEducation;
+    CardView cardViewNemKonto;
+    CardView cardViewMitID;
+    CardView cardViewBenefits;
+    CardView cardViewEboks;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_essentials_tab, container, false);
+
+        cardViewEducation = view.findViewById(R.id.cardEducation);
+        cardViewNemKonto = view.findViewById(R.id.cardNemKonto);
+        cardViewMitID = view.findViewById(R.id.cardMitID);
+        cardViewBenefits = view.findViewById(R.id.cardBenefits);
+        cardViewEboks = view.findViewById(R.id.cardEboks);
+        cardViewEducation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), EducationHomeActivity.class));
+            }
+
+        });
+
+        cardViewNemKonto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), NemKontoPage.class));
+            }
+
+        });
+
+        cardViewMitID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), MitIDpage.class));
+            }
+
+        });
+
+        cardViewBenefits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), BenefitsPage.class));
+            }
+
+        });
+
+        cardViewEboks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), EBoksPage.class));
+            }
+
+        });
 
 
-        return inflater.inflate(R.layout.fragment_essentials_tab, container, false);
 
+    return view;
     }
 
 
--- app/src/main/java/com/example/plswork/EverydayTap.java
+++ app/src/main/java/com/example/plswork/EverydayTap.java
@@ -2,6 +2,7 @@ package com.example.plswork;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -17,10 +18,44 @@ import android.widget.ImageView;
 
 public class EverydayTap extends Fragment {
 
+    CardView cardViewTransport;
+    CardView cardViewHelth;
+    CardView cardViewWork;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_everyday_tap, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_everyday_tap, container, false);
+
+        cardViewTransport = view.findViewById(R.id.cardTransport);
+        cardViewHelth = view.findViewById(R.id.cardHelth);
+        cardViewWork = view.findViewById(R.id.cardWork);
+
+        cardViewTransport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), MainTransport.class));
+            }
+
+        });
+
+        cardViewHelth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), EducationHomeActivity.class));
+            }
+
+        });
+
+        cardViewWork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), EducationOptionsActivity.class));
+            }
+
+        });
+        return view;
     }
 }
