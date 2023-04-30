23b620422d1a180d2a9be9eaf51b30882c257fe0
Miacmr <miachristina15@gmail.com>
2023-04-30 15:00:23 +0200

    Clickable text der laver pop-up i EducationSU

--- app/src/main/java/com/example/plswork/EducationSUActivity.java
+++ app/src/main/java/com/example/plswork/EducationSUActivity.java
@@ -1,15 +1,19 @@
 package com.example.plswork;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class EducationSUActivity extends AppCompatActivity {
 
     Button SUMitIDBtn;
+
+    private TextView educationSUText4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,21 @@ public class EducationSUActivity extends AppCompatActivity {
         SUMitIDBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 startActivity(new Intent(EducationSUActivity.this, MitIDpage.class));
+            }
+        });
+
+        educationSUText4 = findViewById(R.id.EducationSUText4);
+
+        educationSUText4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // create a dialog box or alert dialog to show the information
+                AlertDialog.Builder builder = new AlertDialog.Builder(EducationSUActivity.this);
+                builder.setTitle("How do I apply for SU as a Ukirainian refugee?");
+                builder.setMessage("If you are a refugee from Ukraine, you may apply for equal status with Danish citizens, if you fall within Section 2(2) of the Act on Integration of Aliens in Denmark" +
+                        "\n\nPlease be aware that if you want to apply for equal status with Danish citizens because you fall within Section 2(4) (other aliens granted family reunification) of the Act on Integration of Aliens in Denmark, you must have obtained the right to reside in Denmark according to this rule on 1 July 2019 or later.");
+                builder.setPositiveButton("OK", null);
+                builder.show();
             }
         });
 
