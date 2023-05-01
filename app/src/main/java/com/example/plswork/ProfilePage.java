3f8225cc74210e7a07f4a576a155e824b00a8ae4
athesb <rflamingw@gmail.com>
2023-05-01 08:22:29 +0200

    Dynamic text based on user data

--- app/src/main/java/com/example/plswork/ProfilePage.java
+++ app/src/main/java/com/example/plswork/ProfilePage.java
@@ -1,15 +1,28 @@
 package com.example.plswork;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfilePage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,17 @@ public class ProfilePage extends AppCompatActivity {
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
         Button logOutBtn = (Button) findViewById(R.id.logOutBtn);
+        TextView name = findViewById(R.id.textViewEditName);
+        TextView email = findViewById(R.id.textViewEditEmail);
+        TextView municipality = findViewById(R.id.textViewEditMunicipality);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String userUid = currentUser.getUid();
+        dynamicTextView(userUid, name, "userFullName");
+        dynamicTextView(userUid, email, "userEmail");
+        dynamicTextView(userUid, municipality, "userMunicipality");
+
+
+
 
         logOutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +50,26 @@ public class ProfilePage extends AppCompatActivity {
                 Intent intent = new Intent(ProfilePage.this, LoginActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
+            }
+        });
+    }
+    public void dynamicTextView(String uid, TextView yourView, String path){
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://p8-g1-bc27c-default-rtdb.europe-west1.firebasedatabase.app/");
+        DatabaseReference myRef = database.getReference("users/" + uid);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String text = snapshot.child(path).getValue(String.class);
+                yourView.setText(text);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "Failed to read value.", error.toException());
+
+
             }
         });
     }
