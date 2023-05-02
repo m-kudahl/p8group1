c85ea9f4fccf1a8f3ff8c71e5820258d9a439b94
athesb <rflamingw@gmail.com>
2023-05-02 19:45:22 +0200

    Can receive notification, but still need to store them in database and show them. Reset password made and working.

--- app/src/main/java/com/example/plswork/LoginActivity.java
+++ app/src/main/java/com/example/plswork/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.plswork;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,11 +15,15 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+
+import java.util.ResourceBundle;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -42,6 +49,45 @@ public class LoginActivity extends AppCompatActivity {
         EditText editTextPassword = findViewById(R.id.editTextPassword);
         Button btn = (Button)findViewById(R.id.loginButton);
         Button createBtn = (Button) findViewById(R.id.createaccountButton);
+        Button forgetPasswordBtn = findViewById(R.id.forgotButton);
+
+        forgetPasswordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText resetMail = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset password?");
+                passwordResetDialog.setMessage("Enter your email to receive reset link");
+                passwordResetDialog.setView(resetMail);
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //send reset link to user
+                        String mail = resetMail.getText().toString();
+                        mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(LoginActivity.this, "Reset link sent to your e-mail", Toast.LENGTH_SHORT);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this, "Email not found" + e.getMessage(), Toast.LENGTH_SHORT);
+
+                            }
+                        });
+                    }
+                });
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //close the dialog
+
+                    }
+                });
+                passwordResetDialog.create().show();
+            }
+        });
 
         createBtn.setOnClickListener(new View.OnClickListener() {
              @Override
