a178a433ebf4d662fcf729cb7d4e9928a033ed15
m-kudahl <mathias.laursen97@gmail.com>
2023-05-06 19:04:23 +0200

    Landing Page Buttons Work

--- app/src/main/java/com/example/plswork/LandingActivity.java
+++ app/src/main/java/com/example/plswork/LandingActivity.java
@@ -2,7 +2,10 @@ package com.example.plswork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -11,8 +14,18 @@ public class LandingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
 
+
         //appbar buttons
         AppBarUtility.setupHomeButton(this, R.id.appbarLanding);
         AppBarUtility.setUpBackButton(this);
     }
+        public void goToRegister (View view) {
+            Intent intent = new Intent (this, RegisterActivity.class);
+            startActivity(intent);
+    }
+    public void goToLogin (View view) {
+        Intent intent = new Intent (this, LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
