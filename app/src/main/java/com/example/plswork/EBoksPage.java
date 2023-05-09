bb748691dda0f11eb836126bbc5bd57987810422
athesb <rflamingw@gmail.com>
2023-05-09 10:18:40 +0200

    Lang button added to AppBarPagesActivity.java, ProfilePagesButton added aswell

--- app/src/main/java/com/example/plswork/EBoksPage.java
+++ app/src/main/java/com/example/plswork/EBoksPage.java
@@ -1,7 +1,6 @@
 package com.example.plswork;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -17,7 +16,7 @@ public class EBoksPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eboks_page);
 
-        AppBarUtility.setupLangButton(this, R.id.appbarEBoks);
+        AppBarUtility.setupLangButton(this);
         appleButton = findViewById(R.id.appleStoreButtonEBoks);
         googleButton = findViewById(R.id.googleStoreButtonEBoks);
         appleButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/plswork/EducationOptionsActivity.java
+++ app/src/main/java/com/example/plswork/EducationOptionsActivity.java
@@ -1,17 +1,8 @@
 package com.example.plswork;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class EducationOptionsActivity extends InformationPagesActivity {
 
@@ -34,7 +25,7 @@ public class EducationOptionsActivity extends InformationPagesActivity {
                 showPopUp(v,getString(R.string.educatioOptionsPopUp1),getString(R.string.educatioOptionsPopUp12) + getString(R.string.educatioOptionsPopUp13));
             }
         });
-        AppBarUtility.setupLangButton(this, R.id.textViewEducationOptions);
+        AppBarUtility.setupLangButton(this);
         buttonEducationOptions2 = findViewById(R.id.buttonEducationOptions2);
         buttonEducationOptions2.setOnClickListener(new View.OnClickListener() {
             @Override
